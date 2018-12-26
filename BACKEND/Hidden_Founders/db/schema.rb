545c40664c69383790c9234e48798b59880c0cf9
SKARABI <skarabifounder@gmail.com>
2018-12-26 14:34:51 +0100

    I create a todo List using the  scaffolding

--- BACKEND/Hidden_Founders/db/schema.rb
+++ BACKEND/Hidden_Founders/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180502141347) do
+ActiveRecord::Schema.define(version: 20181226133302) do
+
+  create_table "todo_lists", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
